f6f890fc0d1b2a218fb3c01fe0cac2b210035585
jhony1974 <47429906+jhony1974@users.noreply.github.com>
2022-10-31 18:39:54 -0500

    Create Dollar a pesos.py
    se inlcuye algoritmo para calcular area apartir de dos puntos cartesianos

---
+++ Dollar a pesos.py
@@ -0,0 +1,4 @@
+peso = float(input("Ingresa una cantidad en pesos: "))
+dolar = float(input("Ingresa el precio del dolar: "))
+conversion = peso / dolar
+print(peso," pesos convertidos a dolares es: ",conversion)
